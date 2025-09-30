4e23589931827a88a272154b6af84a6d9dc0c468
Palm <git@palmdevs.me>
2025-09-30 19:52:03 +0700

    oops

--- app/src/main/kotlin/io/github/revenge/xposed/modules/UpdaterModule.kt
+++ app/src/main/kotlin/io/github/revenge/xposed/modules/UpdaterModule.kt
@@ -108,7 +108,7 @@ object UpdaterModule : Module() {
                     // This is a retry, so we show a dialog
                     if (activity != null) {
                         withContext(Dispatchers.Main) {
-                            AlertDialog.Builder(lastActivity).setTitle("Revenge Update Successful")
+                            AlertDialog.Builder(activity).setTitle("Revenge Update Successful")
                                 .setMessage("A reload is required for changes to take effect.")
                                 .setPositiveButton("Reload") { dialog, _ ->
                                     reloadApp()
@@ -139,7 +139,7 @@ object UpdaterModule : Module() {
 
     fun showErrorDialog(e: Throwable) {
         lastActivity?.runOnUiThread {
-            AlertDialog.Builder(activity).setTitle("Revenge Update Failed").setMessage(
+            AlertDialog.Builder(lastActivity).setTitle("Revenge Update Failed").setMessage(
                 """
                 Unable to download the latest version of Revenge.
                 This is usually caused by bad network connection.
@@ -149,8 +149,8 @@ object UpdaterModule : Module() {
             ).setNegativeButton("Dismiss") { dialog, _ ->
                 dialog.dismiss()
             }.setPositiveButton("Retry Update") { dialog, _ ->
-                downloadScript(activity)
-                Toast.makeText(activity, "Retrying download in background...", Toast.LENGTH_SHORT).show()
+                downloadScript(lastActivity)
+                Toast.makeText(lastActivity, "Retrying download in background...", Toast.LENGTH_SHORT).show()
                 dialog.dismiss()
             }.show()
         }
